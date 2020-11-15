a6ddfe93a5f845e303d6b8646e346e7057f777cb
Faure <Joshua.Faure@cslbehring.com.au>
2020-11-15 16:00:59 +1100

    add player info to team info

--- data_prep.R
+++ data_prep.R
@@ -4,11 +4,28 @@ process_all_players = function(){
   all_players <<- readr::read_csv("raw_data/Players 2018-2020.csv") %>% 
     dplyr::mutate(PERSON_ID = PERSON_ID %>% as.character() %>% as.factor())
   
+  brownlow_winners <<- readxl::read_excel("raw_data/brownlow_winners.xlsx")
+    
+  all_players <<- all_players %>% dplyr::mutate(
+    BROWNLOW_WINNER = ifelse(FULLNAME %in% brownlow_winners$Player, 1, 0),
+    AGE_EOS = SEASON_ID - year(DOB)
+  )
+  
+  team_player_summary <<- all_players %>% group_by(SQUAD_NAME, SEASON_ID) %>% 
+    dplyr::summarise(
+      AVG_PLAYER_AGE = mean(AGE_EOS, na.rm = TRUE),
+      AVG_PLAYER_HT = mean(CURRENT_HEIGHT, na.rm = TRUE),
+      AVG_PLAYER_WT = mean(CURRENT_WEIGHT, na.rm = TRUE),
+      BROWNLOW_MEDALISTS = sum(BROWNLOW_WINNER, na.rm = TRUE),
+      AVG_CAREER_MATCHES = mean(CAREER_MATCHES_PRE_2018, na.rm = TRUE)
+    ) %>% ungroup()
+  
   message("Saving all_player data...")
   message("Missing values: ", sum(is.na(all_players)))
   message("Variables with highest proportion of missing values: ")
   print(all_players %>% is.na %>% colMeans %>% sort %>% rev() %>% head())
   saveRDS(object = all_players, file = "./clean_data/all_players.rds")
+  saveRDS(object = team_player_summary, file = "./clean_data/team_player_summary.rds")
 }
 
 process_fixtures = function(){
@@ -65,6 +82,11 @@ process_team_summary = function(){
   team_summary = team_summary_list %>% bind_rows(.id = "file_name") %>% 
     dplyr::mutate(MATCH_ID = MATCH_ID %>% as.character() %>% as.factor()) %>% 
     left_join(fixtures, by = c("MATCH_ID", "SEASON_ID", "GROUP_ROUND_NO", "VENUE_NAME"))
+  
+  opp_team_player_summary = team_player_summary %>% dplyr::rename_all(function(x) paste0("OPP_", x))
+  
+  team_summary = team_summary %>% dplyr::left_join(team_player_summary, by = c("SEASON_ID" = "SEASON_ID", "SQUAD_NAME" = "SQUAD_NAME")) %>% 
+    dplyr::left_join(opp_team_player_summary, by = c("SEASON_ID" = "OPP_SEASON_ID", "OPP_SQUAD_NAME" = "OPP_SQUAD_NAME"))
   
   message("Missing values: ", sum(is.na(team_summary)))
   message("Saving team_summary data...")
